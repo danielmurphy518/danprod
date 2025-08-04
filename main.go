e05db8823d773beb9110227ee15993be3457546d
daniel murphy <daniel.murphyx@gmail.com>
2025-08-04 23:53:07 +1000

    feat(main): very basic commands

--- main.go
+++ main.go
@@ -1,13 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 )
 
+func openURL(url string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", url)
+	case "darwin": 
+		cmd = exec.Command("open", url)
+	default: 
+		cmd = exec.Command("xdg-open", url) 
+	}
+
+	return cmd.Start() 
+}
+
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 
-	baseCommand := os.Args[1]
+	fmt.Println("Enter something (type 'exit' to quit):")
 
-    fmt.Println(baseCommand)
+	for {
+		fmt.Print("> ")
+		if !scanner.Scan() {
+			break 
+		}
+		input := strings.TrimSpace(scanner.Text())
+
+		commands := strings.Split(input, " ")
+		fmt.Printf("Commands entered: %v\n", commands)
+		fmt.Printf("Type of var: %T\n", commands)
+
+		if input == "exit" {
+			fmt.Println("Goodbye!")
+			break
+		}
+
+		fmt.Println("You entered:", input)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+	}
 }
